80eda69407614c54dc1e2d6b14b68ccd33a83489
ConnieFisher <ConnieFisher@users.noreply.github.com>
2017-11-10 11:42:02 -0500

    added imports to app.module

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,109 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common' ;
-import { MatButtonModule, MatToolbarModule } from '@angular/material' ;
+// tslint:disable-next-line:max-line-length
+import { MatButtonModule,
+    MatAccordion,
+    MatProgressBar,
+    MatDialog,
+    MatTooltip,
+    MatSnackBar,
+    MatCalendar,
+    MatSortHeader,
+    MatPaginatorIntl} from '@angular/material' ;
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 @NgModule({
-    imports: [MatButtonModule, MatToolbarModule],
-    exports: [MatButtonModule, MatToolbarModule],
+    imports: [
+        MatButtonModule,
+        MatToolbarModule,
+        MatAutocompleteModule,
+        MatCheckboxModule,
+        MatDatepickerModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatRadioModule,
+        MatSelectModule,
+        MatSliderModule,
+        MatMenuModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatListModule,
+        MatGridListModule,
+        MatCardModule,
+        MatStepperModule,
+        MatTabsModule,
+        MatAccordion,
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatChipsModule,
+        MatIconModule,
+        MatProgressSpinnerModule,
+        MatProgressBar,
+        MatDialog,
+        MatTooltip,
+        MatSnackBar,
+        MatCalendar,
+        MatSortHeader,
+        MatPaginatorIntl
+    ],
+    exports: [
+        MatButtonModule,
+        MatToolbarModule,
+        MatAutocompleteModule,
+        MatCheckboxModule,
+        MatDatepickerModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatRadioModule,
+        MatSelectModule,
+        MatSliderModule,
+        MatMenuModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatListModule,
+        MatGridListModule,
+        MatCardModule,
+        MatStepperModule,
+        MatTabsModule,
+        MatAccordion,
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatChipsModule,
+        MatIconModule,
+        MatProgressSpinnerModule,
+        MatProgressBar,
+        MatDialog,
+        MatTooltip,
+        MatSnackBar,
+        MatCalendar,
+        MatSortHeader,
+        MatPaginatorIntl]
 })
 export class MaterialModule { }
